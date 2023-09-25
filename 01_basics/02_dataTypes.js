ad1481648c6da0f38139be682866c60101d0623d
Shailesh Kumar <64634672+Shailesh3122@users.noreply.github.com>
2023-09-25 07:29:52 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -3,9 +3,9 @@
 //alert(3+3);  // we are using nodejs, not browser
 
 console.log(3+
-    3)          //code readability should be high
+    3);          //code readability should be high
 
-console.log("Shailesh")
+console.log("Shailesh");
 
 
 
@@ -14,19 +14,22 @@ let age = 27;
 let isLoggedIn = true;
 let state;
 
-// number
-//bigint
+// primitive data types =>
+// number => 2 to the power of 53
+//bigint    => larger than number
 //string => ""
 //boolean => true/false
 //null => standalone value
 //undefined
 //symbol => uniqueness
 
-
-//object
+//reference data types
+//array, function, object 
 
 console.log(typeof name2);  // string
+console.log(typeof age);    //number
 console.log(typeof isLoggedIn); // boolean
 console.log(typeof state);  // undefined
 console.log(typeof null);  // object
-
+console.log(typeof undefined); //undefined
+console.log(typeof NaN);    //number
