d506ad63a4c552c5c4b99dc6e802bd6c2b936e88
Shailesh Kumar <64634672+Shailesh3122@users.noreply.github.com>
2023-09-25 08:19:43 +0000

    comparison

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,16 +1,58 @@
-console.log(2>1);
-console.log(2>=1);
+console.log(2>1);   //true
+console.log(2>=1);  //true
 
-console.log("2">1);
-console.log("2abc">1);
+console.log("2">1); //true
+console.log("2abc">1);  //false
 
-console.log(null>0);
-console.log(null==0);
-console.log(null>=0);
+console.log(null>0);   //false
+console.log(null==0);   //false
+console.log(null>=0);   //true because comparisons convert null to a number, treating it as 0.
 
-console.log(undefined>0);
-console.log(undefined==0);
+console.log(undefined>0);   //false
+console.log(undefined==0);  //false
+
 
 // === check datatypes and value also
 
-console.log("2" ===2);
+console.log("2" ===2);  //false
+
+
+
+// falsy values=>
+
+
+// if (false) {
+//     // Not reachable
+//   }
+  
+//   if (null) {
+//     // Not reachable
+//   }
+  
+//   if (undefined) {
+//     // Not reachable
+//   }
+  
+//   if (0) {
+//     // Not reachable
+//   }
+  
+//   if (-0) {
+//     // Not reachable
+//   }
+  
+//   if (0n) {
+//     // Not reachable
+//   }
+  
+//   if (NaN) {
+//     // Not reachable
+//   }
+  
+//   if ("") {
+//     // Not reachable
+//   }
+
+//   if (document.all) {
+//     // Not reachable
+//   }
